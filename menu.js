f8e4847a9219d9b61c127214a39d61009a757f5b
larz <rey_gato10@hotmail.com>
2020-04-29 15:31:11 -0400

    menu echo con jquery

---
+++ menu.js
@@ -0,0 +1,17 @@
+$(document).ready(main);
+var contador=1;
+function main(){
+	$('.menu_bar').click(function(){
+		if(contador==1){
+		$('nav').animate({
+			left:"0"
+		});
+		contador=0;
+		}else{
+			contador=1;
+			$('nav').animate({
+				left:'-100%'
+			})
+		}
+	});
+};
